c0637dd38c6e20b66180c5e4244db7145f8ed8b7
Thiago <thiagokriegercardoso@gmail.com>
2021-04-15 02:29:25 -0300

    Primeiro Programa de Phyton

---
+++ Primeiro_Projeto.py
@@ -0,0 +1,13 @@
+print("Hello World, This is my first program on python,Thiago K")
+print("Band name generator program")
+print("What's the name of the city that you grew up in?")
+cidade = input()
+print("Whats your pet name?")
+pet_name = input()
+print("Your band name could be: " + cidade + " "  +pet_name)
+
+
+
+
+print("Exercício feito por Thiago K")
+input("prompt: ")
