81555f89a19331e5f0e4eb5fe9ab77c75151a8ea
Thiago <thiagokriegercardoso@gmail.com>
2021-04-17 19:50:57 -0300

    # Calculadora de Soma Simples Programação Phyton #

---
+++ calculadora_soma.py
@@ -0,0 +1,6 @@
+print("Bem vindo a minha primeira calculadora com programação Phyton! By KriegerThiago")
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+resultado = float(num1) + float(num2)
+
+print("O resultado da SOMA dos números é: " + format(resultado,".2f"))
